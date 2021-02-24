074c72b4e2f363f62800f667666fe9046b2f4baa
Stephen Schaub <sschaub@bju.edu>
2021-02-24 17:28:32 -0500

    Update

--- runtest.sh
+++ runtest.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 
-echo $GITHUB_REPOSITORY
+echo $GITHUB_REPOSITORY > log.txt
 
 project=$(echo $GITHUB_REPOSITORY | cut -d/ -f2 | cut -d- -f2)
 
-echo Checking $project
-echo Current dir is $(pwd)
-ls -l
-python3 -v
+if [ -e $project ]
+then
+    pushd $project
+    if bash $project.sh >>log.txt 2>&1
+    then
+        echo Success
+    else
+        echo Failure
+    fi
+    popd
+fi
 
+echo > submission/README.md <<<EOF
+# README
+
+Test results for submission at $(date)
+```
+$(cat log.txt)
+```
+EOF
+
+cd submission
+git config --local user.email "action@github.com"
+git config --local user.name "GitHub Action"
+git add README.md 
+git commit -m "Add/Update badge"
