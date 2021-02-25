ae21e5b499f32c3636627985aaa20adc8b5bc745
Stephen Schaub <sschaub@bju.edu>
2021-02-24 19:11:42 -0500

    Update

--- runtest.sh
+++ runtest.sh
@@ -4,28 +4,34 @@ echo $GITHUB_REPOSITORY > log.txt
 
 project=$(echo $GITHUB_REPOSITORY | cut -d/ -f2 | cut -d- -f2)
 
+image=checkmark.svg
 if [ -e $project ]
 then
     pushd $project
-    if bash runtests.sh >>log.txt 2>&1
+    if bash runtests.sh >>../log.txt 2>&1
     then
-        echo Success
+        echo Success        
     else
         echo Failure
+        image=fail.svg
     fi
     popd
 fi
 
-cat > log.md <<EOF
-# README
+cat > ../submission/README.md <<EOF
+# Submission Status
 
 Test results for submission at $(date)
 
+![](https://raw.githubusercontent.com/sschaub/ssdemo/master/.github/$image)
+```
+$(cat log.txt)
+```
 EOF
 
-# cd ../submission
-# git config --local user.email "action@github.com"
-# git config --local user.name "GitHub Action"
-# git add README.md 
-# git commit -m "Automatic Tester Results"
-# git push
+cd ../submission
+git config --local user.email "action@github.com"
+git config --local user.name "GitHub Action"
+git add README.md 
+git commit -m "Automatic Tester Results"
+git push
