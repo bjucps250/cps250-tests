38dbb6d198c4bf0060b9e615d41ea3c1a784f8f7
Stephen Schaub <sschaub@bju.edu>
2021-03-16 17:32:33 -0400

    Update

--- exercise5/_runtests.sh
+++ exercise5/_runtests.sh
@@ -4,6 +4,8 @@ exit-if-must-pass-tests-failed
 
 require-pdf report.pdf
 
+cp $TEST_DIR/readblah.so .
+
 run-program --test-message "findconn.py executes with no errors" --showoutputonpass python3 findconn.py $TEST_DIR/testdata
 
 if [ -e findconn_xc.py ]; then
