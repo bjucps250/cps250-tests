a5d6cfb114d3632e8bd32260f6619b22df6d604f
Stephen Schaub <sschaub@bju.edu>
2021-04-20 12:44:34 -0400

    Update exercise 5 test

--- exercise5/_runtests.sh
+++ exercise5/_runtests.sh
@@ -9,7 +9,7 @@ cp $TEST_DIR/readblah.so .
 run-program --test-message "findconn.py executes with no errors" --showoutputonpass python3 findconn.py $TEST_DIR/testdata
 
 if [ -e findconn_xc.py ]; then
-  run-program --test-message "findconn_xc.py executes with no errors" --showoutputonpass python3 findconn.py $TEST_DIR/testdata
+  run-program --test-message "findconn_xc.py executes with no errors" --showoutputonpass python3 findconn_xc.py $TEST_DIR/testdata
 else
   echo "No findconn_xc.py found..."
 fi
